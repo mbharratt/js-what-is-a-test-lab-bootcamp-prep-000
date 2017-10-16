c94326b157942409c63799ab1aa154931630d468
mbharratt <melissa.bharratt@gmail.com>
2017-10-16 17:51:39 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -17,7 +17,7 @@ describe('what-is-a-test', () => {
   describe('Message', () => {
     it('gives the name and height', () => {
       expect(index.message).toInclude(index.Susan)
-      expect(index.message).toInclude(index."39")
+      expect(index.message).toInclude(index.39)
     })
   })
 
